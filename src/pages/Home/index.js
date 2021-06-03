0f1ddd9ac0ce53326534a81eeb0ddfc60aeaf753
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2021-06-03 03:25:43 -0300

    Home Finalizada

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,49 @@
 import React from 'react';
-import {View, Text} from 'react-native';
 import {LinearGradient} from 'expo-linear-gradient';
 import StatusBarPage from '../../components/StatusBarPage';
+import Menu from '../../components/Menu';
+import {Feather} from '@expo/vector-icons';
+import { ContainerLogo, Logo, ContainerContent, Title, Subtitle,
+ContainerInput, BoxIcon, Input, ButtonLink, ButtonLinkText} from './style';
 
 export default function Home(){
     return(
         <LinearGradient
             colors={['#1ddbb9','#132742']}
-            style={{flex:1, justifyContent:'center', alignItems:'center'}}
+            style={{flex:1, justifyContent:'center'}}
         >
             <StatusBarPage
                 barStyle="ligth-content"
                 backgroundColor="#1ddbb9"
             />
             
-            <Text>Pagina Home</Text>
+            <Menu/>
+
+            <ContainerLogo>
+                <Logo source={require('../../assets/Logo.png')} resizeMode="contain"/>
+            </ContainerLogo>
+
+            <ContainerContent>
+                <Title>SujeitoLink</Title>
+                <Subtitle>Cole seu link para encurtar</Subtitle>
+            
+                <ContainerInput>
+                    <BoxIcon>
+                        <Feather name="link" size={22} color="#fff" />
+                    </BoxIcon>
+                    <Input
+                        placeholder="Cole seu link aqui..."
+                        placeholderTextColor="#fff"
+                    />
+                </ContainerInput>
+
+                <ButtonLink>
+                    <ButtonLinkText>Gerar Link</ButtonLinkText>
+                </ButtonLink>
+
+            </ContainerContent>
+
+
         </LinearGradient>
     )
 }
